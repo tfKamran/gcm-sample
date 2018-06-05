b82f96cb6cea2f5d0125f0b9eace790ce9f7c98a
Kamran Ahmed Ansari <team.radiant@gmail.com>
2018-06-05 11:18:54 +0530

    Add an listener to be able to update token on the UI as soon as it is refreshed

--- gcm-sample-android/app/src/main/java/com/tf/gcmsample/MyFirebaseInstanceIDService.java
+++ gcm-sample-android/app/src/main/java/com/tf/gcmsample/MyFirebaseInstanceIDService.java
@@ -1,9 +1,14 @@
 package com.tf.gcmsample;
 
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by kamran on 18/03/17.
@@ -13,11 +18,13 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
     public static final String PREF_INSTANCE_ID = "instance_id";
 
+    private static List<OnTokenRefreshListener> sListeners = new ArrayList<>();
+
     @Override
     public void onTokenRefresh() {
         super.onTokenRefresh();
 
-        String instanceId = FirebaseInstanceId.getInstance().getToken();
+        final String instanceId = FirebaseInstanceId.getInstance().getToken();
 
         if (instanceId != null) {
             SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
@@ -25,6 +32,28 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
             sharedPreferences.edit()
                     .putString(PREF_INSTANCE_ID, instanceId)
                     .apply();
+
+            final Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    for (OnTokenRefreshListener listener : sListeners) {
+                        listener.onTokenRefresh(instanceId);
+                    }
+                }
+            });
         }
+    }
+
+    public static void addOnTokenRefreshListener(OnTokenRefreshListener listener) {
+        sListeners.add(listener);
+    }
+
+    public static void removeOnTokenRefreshListener(OnTokenRefreshListener listener) {
+        sListeners.remove(listener);
+    }
+
+    public interface OnTokenRefreshListener {
+        void onTokenRefresh(String instanceId);
     }
 }
